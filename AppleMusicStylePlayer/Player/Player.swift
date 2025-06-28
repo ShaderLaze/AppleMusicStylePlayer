1217937b9f1defcc10f642a9c866ff84faf35942
ShaderLaze <sasha-galperov@yandex.ru>
2025-06-28 18:26:56 +0600

    Implement AVAudioPlayer playback

--- AppleMusicStylePlayer/Player/Player.swift
+++ AppleMusicStylePlayer/Player/Player.swift
@@ -5,14 +5,32 @@
 //  Created by Alexey Vorobyov on 30.11.2024.
 //
 
+import AVFoundation
 import Foundation
 
 class Player {
+    private var player: AVAudioPlayer?
+
+    init() {
+        try? AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+        try? AVAudioSession.sharedInstance().setActive(true)
+    }
+
     func play(_ media: Media) {
-        print("Play \(media.title)")
+        guard let url = media.fileURL else {
+            print("Media has no file URL")
+            return
+        }
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player?.prepareToPlay()
+            player?.play()
+        } catch {
+            print("Failed to play", error)
+        }
     }
 
     func stop() {
-        print("Stop")
+        player?.stop()
     }
 }
